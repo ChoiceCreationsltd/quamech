8fd22c7fb096b01a3d48e7b81b1a333bb25a8f0d
joshua-inf <jsibanda407@gmial.com>
2023-08-18 16:57:48 +0200

    contact us

--- src/Pages/Conatctus.js
+++ src/Pages/Conatctus.js
@@ -1,5 +1,6 @@
 import image from '../Images/close-up-metallic-equipment-part-turning-factory-generated-by-ai.jpg'
 import {BsHeadset} from 'react-icons/bs'
+import {IoMdSend} from 'react-icons/io'
 import {FaMapMarkerAlt} from 'react-icons/fa'
 import {AiOutlineMail} from 'react-icons/ai'
 const Contact = () => {
@@ -15,11 +16,11 @@ const Contact = () => {
                                 <div className='col-1'></div>
                                 <div className='col-10'>
                                     <div className='row'>
-                                        <div className='col-md-6 col-12'>
+                                        <div className='col-md-4 col-12'>
                                             <div className='py-4'>
                                                 <div className='row'>
                                                     <div className='col-12 p-2'>
-                                                        <div className='border-primary border rounded  p-3 d-flex' >
+                                                        <div className='border-primary contx border rounded  p-3 d-flex' >
                                                             <div className='d-flex justify-content-center border-danger me-2 flex-column p-3' style={{borderRight:"5px solid"}}><BsHeadset size={50} /></div>
                                                             <div>
                                                                 <div style={{fontSize:'11px'}}>
@@ -39,7 +40,7 @@ const Contact = () => {
                                                         </div>
                                                     </div>
                                                     <div className='col-12 p-2'>
-                                                        <div className='border-danger border rounded  p-3 d-flex'>
+                                                        <div className='border-danger contx border rounded  p-3 d-flex'>
                                                             <div className='d-flex justify-content-center border-danger me-2 flex-column p-3 ' style={{borderRight:"5px solid"}}><FaMapMarkerAlt className='' size={50} /></div>
                                                             <div>
                                                                 <div style={{fontSize:'11px'}}>
@@ -54,7 +55,7 @@ const Contact = () => {
                                                         </div>
                                                     </div>
                                                     <div className='col-12 p-2'>
-                                                        <div className='border-primary border rounded  p-3 d-flex'>
+                                                        <div className='border-primary contx border rounded  p-3 d-flex'>
                                                             <div className='d-flex justify-content-center border-danger me-2 flex-column p-3 ' style={{borderRight:"5px solid"}}><AiOutlineMail className='' size={50} /></div>
                                                             <div>
                                                                 <div className='text-start' style={{fontSize:'11px'}}>
@@ -78,9 +79,24 @@ const Contact = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className='col-md-6 col-12'>
+                                        <div className='col-md-8 col-12'>
                                             <div className='py-5'>
-                                                <input className='border border-primary' style={{backgroundColor:'none', }}/>
+                                                <form>
+                                                    <div className='d-flex' style={{gap:'20px'}}>
+                                                        <div className='w-100 text-start'>
+                                                            <div>Email:</div>
+                                                            <input required type='email' className='border border-primary text-white p-2 w-100' style={{backgroundColor:'rgba(0,0,0,0)' }}/>
+                                                        </div>
+                                                        <div className='w-100 text-start'>
+                                                            <div>Subject (optional):</div>
+                                                            <input type='text' placeholder='' className='border border-primary text-white p-2 w-100' style={{backgroundColor:'rgba(0,0,0,0)' }}/>
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <textarea  style={{resize:'none',backgroundColor:'rgba(0,0,0,0)', minHeight:'300px'}} className='border text-white p-2 border-primary w-100 my-4' />
+                                                    </div>
+                                                    <button className='btn btn-outline-primary text-white rounded-0 w-50 '>Send <IoMdSend className='text-white'  size={20}/></button>
+                                                </form>
                                             </div>
                                         </div>
                                     </div>
